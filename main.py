b658993b2c16c01b7846fecbf2681f601c24cd11
ApurboShib <shibjoy088@gmail.com>
2026-03-16 20:46:53 +0600

    feat: Add an endpoint to sort patient data by height, weight, or BMI.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 
@@ -41,3 +41,24 @@ def view_patients(patients_id: str = Path(..., description = "ID of the patients
     if patients_id in data:
         return  data[patients_id]
     raise HTTPException(status_code = 404, detail = "Patients is not found!")
+
+
+
+## create a new endpoints to see informattions in sorted manner.
+
+@app.get("/sort")
+def sort_patients(sort_by:str = Query(..., description = "sort on the basis of height, weight and BMI"), order :str = Query('asc', description = "sort in the ASC or DESC order")):
+    ## the error handeling part.. (for which case it might gives error).
+    valid_feilds  = ['height', 'weight', 'bmi']
+    valid_order = ['ASE', 'DESC']
+    if sort_by not in valid_feilds:
+        raise HTTPException(status_code = 400, detail = f'invalid feilds selected from {valid_feilds}')
+    if order not in valid_order:
+        raise HTTPException(status_code = 400, detail = 'Invalid order selected please select ASC or DESC order')
+## first of all we retieved the data first.
+    data = load_data()
+
+    sorted_ordder = True if order == 'ASC' else False
+
+    sorted_data = sorted(data.values(), key = lambda x : x.get(sort_by, 0), reverse = (sorted_ordder))
+    return sorted_data
